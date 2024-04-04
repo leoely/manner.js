d56a290ce5739d45f4d4286d686c2c94f2e2329c
Leo Brad <>
2024-04-04 17:06:12 +0800

    [Import] Import new manner.js package

--- server/lib/parseDateString.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = parseDateString;
-function parseDateString(dateString) {
-  return new Date(httpDate).getTime();
-}
