285332a0a8fb428c6dfdbe5838271293ce562889
Leo Brad <>
2024-04-06 17:28:22 +0800

    [Change] Import filterNamespace path.

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
-export { default as filterNamespace, } from '~/client/lib/filename';
+export { default as filterNamespace, } from '~/client/lib/filterNamespace';
 export { default as clearCookie, } from '~/client/lib/setCookie';
 export { default as setCookie, } from '~/client/lib/setCookie';
 export { default as readCookie, } from '~/client/lib/setCookie';
