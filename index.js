a3d6aafd59b7ef62349deecc47d48b0d44c7e7fe
Leo Ely <>
2025-06-02 17:52:02 +0800

    [Publish] Publish version v1.0.0

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _MultiFetch = _interopRequireDefault(require("./server/class/MultiFetch"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+console.log(_MultiFetch.default);
