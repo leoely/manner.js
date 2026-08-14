6de694487e24934ef21e6cde831431c51bebebe4
Leo Ely <leoely789@gmail.com>
2026-08-14 16:46:11 +0800

    [Adaptation] Adaptation method hostname information.

--- src/server/lib/getAddress.js
+++ src/server/lib/getAddress.js
@@ -1,12 +1,16 @@
 import net from 'net';
 
-export default function getAddress(ip, port) {
-  if (net.isIPv6(ip)) {
-    const ipv6 = ip;
+export default function getAddress(hostname, port) {
+  if (net.isIPv6(hostname)) {
+    const ipv6 = hostname;
     return '[' + ipv6 + ']:' + port;
   }
-  if (net.isIPv4(ip)) {
-    const ipv4 = ip;
+  if (net.isIPv4(hostname)) {
+    const ipv4 = hostname;
     return ipv4 + ':' + port;
   }
+  if (/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(hostname)) {
+    return hostname + ':' + port;
+  }
+  throw new Error('[Error] The parameter hostname should belong to one of the set{ipv4, ipv6, hostname}.');
 }
--- test/server/lib/getAddress.test.js
+++ test/server/lib/getAddress.test.js
@@ -5,5 +5,7 @@ describe('[lib] getAddress;', () => {
   test('The output of the function getAddress should be correct.', () => {
     expect(getAddress('192.168.2.5', 80)).toMatch('192.168.2.5:80');
     expect(getAddress('2001:db8:85a3:8d3:1319:8a2e:370:7348', 80)).toMatch('[2001:db8:85a3:8d3:1319:8a2e:370:7348]:80');
+    expect(getAddress('www.manner.com', 80)).toMatch('www.manner.com:80');
+    expect(() => getAddress('fdsf$3423asd3243', 80)).toThrow('[Error] The parameter hostname should belong to one of the set{ipv4, ipv6, hostname}.');
   });
 });
